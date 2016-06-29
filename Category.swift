2a5c7f8e51cd6a22529409a86ae866be8db0e11f
Austin Tooley <tooley7@gmail.com>
2016-06-29 10:44:13 -0500

    Saves json via archiving on load. If no network, uses saved data

--- Category.swift
+++ Category.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Category {
+class Category: NSObject, NSCoding {
     let id: Int
     var name: String
     var desc: String
@@ -18,5 +18,17 @@ class Category {
         self.id = id
         self.name = name
         self.desc = desc
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        id = aDecoder.decodeIntegerForKey("id")
+        name = aDecoder.decodeObjectForKey("name") as! String
+        desc = aDecoder.decodeObjectForKey("desc") as! String
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeInteger(id, forKey: "id")
+        aCoder.encodeObject(name, forKey: "name")
+        aCoder.encodeObject(desc, forKey: "desc")
     }
 }
--- Repository.swift
+++ Repository.swift
@@ -9,9 +9,11 @@
 import Foundation
 
 class Repository {
+    
+    
     func fetchAllCategories(success fs: [Category] -> Void, failure ff: () -> Void) {
        
-        let url = "http://svc.treeloop.net/product"
+        let url = "http://svc.treeloop.netR/product"
         let request = NSMutableURLRequest(URL: NSURL(string: url)!)
         let session = NSURLSession.sharedSession()
         
@@ -20,7 +22,11 @@ class Repository {
             
             //check for error
             if error != nil || data == nil {
-                ff()
+                if let localCategories = self.loadCategories() {
+                    fs(localCategories)
+                } else {
+                    ff()
+                }
                 return
             }
             //process data
@@ -33,8 +39,45 @@ class Repository {
                     desc: subJson["Description"].stringValue)
                 categories.append(c)
             }
+            self.saveCategories(categories)
             fs(categories)
         })
         task.resume()
     }
+    
+    private func saveCategories(categories: [Category]) -> Bool {
+        let filemgr = NSFileManager.defaultManager()
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let path = "\(dirPaths[0])/Northwinder.data"
+        
+        let data = NSKeyedArchiver.archivedDataWithRootObject(categories)
+        
+        if filemgr.fileExistsAtPath(path) {
+            do {
+                try filemgr.removeItemAtPath(path)
+            } catch {
+                return false
+            }
+        }
+        return filemgr.createFileAtPath(path, contents:data, attributes:nil)
+    }
+    
+    private func loadCategories() -> [Category]? {
+        let filemgr = NSFileManager.defaultManager()
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let path = "\(dirPaths[0])/Northwinder.data"
+        
+        guard filemgr.fileExistsAtPath(path) else { return nil }
+        
+        if let data = filemgr.contentsAtPath(path) {
+            if let categories = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? [Category] {
+                return categories
+            }
+        }
+        return nil
+    }
 }
+
+
+
+
